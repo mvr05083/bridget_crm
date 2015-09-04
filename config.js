535c03ac175c955edba9c3b09606e3cd0f7e0963
Mike Redmond <mkdeveloper56@gmail.com>
2015-09-04 15:33:00 -0400

    Fix port error

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-	'port' : process.env.PORT || 8080,
+	'port' : process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	// 'database' : 'mongodb://localhost:27017/sjfc_crm',
 	'database' : 'mongodb://crm_janitor:Ghostadmin3@ds035683.mongolab.com:35683/bcrm',
 	'secret' : 'Vj1QdrWifYFKbrLM99woJ2JhHSm/ufBfjQPsjH+WH+k='
 }
- 
