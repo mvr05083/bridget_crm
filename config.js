6092d37cc17be63d00131b665d002cd7a99c6162
Mike Redmond <mkdeveloper56@gmail.com>
2015-09-08 10:33:40 -0400

    Sync config.js

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
-	'port' : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002,
+	'port' : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
 	'ip' : process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
-=======
-	'port' : process.env.PORT || 8080,
->>>>>>> 589d63a1ba52fc9190f7477b84b707509411a004
 	// 'database' : 'mongodb://localhost:27017/sjfc_crm',
 	'database' : 'mongodb://crm_janitor:Ghostadmin3@ds035683.mongolab.com:35683/bcrm',
 	'secret' : 'Vj1QdrWifYFKbrLM99woJ2JhHSm/ufBfjQPsjH+WH+k='
 }
-<<<<<<< HEAD
-=======
- 
->>>>>>> 589d63a1ba52fc9190f7477b84b707509411a004
