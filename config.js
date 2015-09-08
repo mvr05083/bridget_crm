3362824b4c935e2b6a772fcceca6fdf66299e2f5
Mike Redmond <mkdeveloper56@gmail.com>
2015-09-08 11:24:58 -0400

    Revert to normal

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-	'port' : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
+	'port' : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002,
 	'ip' : process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
 	// 'database' : 'mongodb://localhost:27017/sjfc_crm',
 	'database' : 'mongodb://crm_janitor:Ghostadmin3@ds035683.mongolab.com:35683/bcrm',
 	'secret' : 'Vj1QdrWifYFKbrLM99woJ2JhHSm/ufBfjQPsjH+WH+k='
 }
- 
